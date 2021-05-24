59de86c73351d03ab1ebf18a7923dc523d7f3ea4
Ivan Dimanov <ivan.dimanov@xogito.com>
2021-05-24 09:05:39 +0300

    E2E config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  preset: 'jest-playwright-preset',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
   moduleNameMapper: {
     'transform': {
       '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
     },
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['./jest.setup.js'],
 }
