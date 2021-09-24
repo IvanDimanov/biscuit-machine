f73458f7e6cb345564399681cdfb33769e49e86b
Ivan Dimanov <Ivan.Dimanov@movedigital.ch>
2021-09-24 12:36:05 +0300

    run id

--- .github/js-scripts/download-artifact.js
+++ .github/js-scripts/download-artifact.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const { Octokit } = require('@octokit/core')
 const admZip = require('adm-zip')
 
@@ -26,36 +25,15 @@ const archiveFormat = 'zip'
 
   const artifact = artifacts.find(({ name }) => name === `${artifactName}-${GITHUB_SHA}`)
 
-  console.log( artifact )
-
-
-  const { data, headers } = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
+  const { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
     owner,
     repo,
     artifact_id: artifact.id,
     archive_format: archiveFormat,
   })
-
-  console.log( data )
-  console.log( Buffer.from(data) )
-  console.log( Buffer.from(data).toString() )
-  console.log( Buffer.from(data).toString('utf-8') )
-  console.log( headers )
-
-
-  fs.writeFileSync(`./${artifactName}.${archiveFormat}`, Buffer.from(data))
-
-  console.log('finished downloading')
-  const zip = new admZip(Buffer.from(data))
   
+  const zip = new admZip(Buffer.from(data))
   zip.getEntries().forEach((zipEntry) => {
-    console.log('entryName =', zipEntry.entryName)
-    console.log('toString', zipEntry.toString())
     console.log('content', zipEntry.getData().toString('utf8'))
   })
-
-  console.log('start unzip')
-  zip.extractAllTo(`./admin-${artifactName}`, true)
-
-  console.log('finished unzip')
 })()
