14c7bb9a5fb537fc509b1a4bef4cdd63c4360cab
Ivan Dimanov <Ivan.Dimanov@movedigital.ch>
2021-09-24 11:24:24 +0300

    Test #8

--- .github/js-scripts/download-artifact.js
+++ .github/js-scripts/download-artifact.js
@@ -5,7 +5,6 @@ const artifactName = process.argv[3]
 
 const {
   GITHUB_REPOSITORY,
-  GITHUB_RUN_ID,
   GITHUB_SHA,
 } = process.env
 
@@ -21,4 +20,15 @@ const {
   const artifact = artifacts.find(({ name }) => name === `${artifactName}-${GITHUB_SHA}`)
 
   console.log( artifact )
+
+
+  const { data, headers } = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/{archive_format}', {
+    owner,
+    repo,
+    artifact_id: artifact.id,
+    archive_format: 'zip',
+  })
+
+  console.log( data )
+  console.log( headers )
 })()
